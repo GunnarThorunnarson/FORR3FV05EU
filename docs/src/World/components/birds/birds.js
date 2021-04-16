1f95d75cd59d9b4ce1f9d6c86aba3f6c904557dc
Gunnar Þórunnarson <gus@tskoli.is>
2021-04-16 12:19:12 +0000

    Add files via upload

---
+++ docs/src/World/components/birds/birds.js
@@ -0,0 +1,15 @@
+import { GLTFLoader } from '../../../../vendor/GLTFLoader.js';
+import { setupModel } from './setupModel.js';
+
+async function loadBirds() {
+  const loader = new GLTFLoader();
+  const parrotData = await loader.loadAsync('/assets/models/Parrot.glb');
+  console.log('Squaaawk!', parrotData);
+  // pass in the loaded data and get back the bird model
+  const parrot = setupModel(parrotData);
+  parrot.position.set(0, 0, 2.5);
+
+  return { parrot }
+}
+
+export { loadBirds };
---
+++ docs/src/World/components/birds/setupModel.js
@@ -0,0 +1,9 @@
+function setupModel(data) {
+    // vísum í parrot object
+    const model = data.scene.children[0];
+
+    return model;
+  
+}
+
+export { setupModel };
