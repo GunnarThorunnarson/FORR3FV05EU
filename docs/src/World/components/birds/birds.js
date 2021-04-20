9be817cbc14646d9149474d455835a2fa019d812
Gunnar Þórunnarson <gus@tskoli.is>
2021-04-20 22:32:38 +0000

    Update birds.js

--- docs/src/World/components/birds/birds.js
+++ docs/src/World/components/birds/birds.js
@@ -4,7 +4,7 @@ import { setupModel } from './setupModel.js';
 async function loadBirds() {
   const loader = new GLTFLoader();
   
-   const parrotData = await loader.loadAsync('https://raw.githubusercontent.com/GunnarThorunnarson/FORR3FV05EU/main/assets/models/Parrot.glb');
+   const parrotData = await loader.loadAsync('https://raw.githubusercontent.com/GunnarThorunnarson/FORR3FV05EU/master/assets/models/Parrot.glb');
   // const parrotData = await loader.loadAsync('/assets/models/Parrot.glb');
   console.log('Squaaawk!', parrotData);
   // pass in the loaded data and get back the bird model
